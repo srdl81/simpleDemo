1af565fb312779d72a075afe245a2cb4e811eb81
xelikan <xelikan007@gmail.com>
2016-10-17 15:05:51 +0200

    Error handling for ids

--- src/main/java/com/ams/hack/exception/NotFoundException.java
+++ src/main/java/com/ams/hack/exception/NotFoundException.java
@@ -4,9 +4,11 @@ package com.ams.hack.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Duration time with provided ids could not be found")
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Duration time could not be fetched.")
 public class NotFoundException extends RuntimeException {
+
     public NotFoundException(String originId, String destinationId) {
         super(String.format("Duration time could not be found with originId '%s' destinationId '%s'", originId, destinationId));
     }
+
 }
