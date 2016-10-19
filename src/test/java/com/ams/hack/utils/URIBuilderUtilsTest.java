6be7a56a938e6995b10ce288a016e4980732cd54
xelikan <xelikan007@gmail.com>
2016-10-20 00:16:25 +0200

    final static

--- src/test/java/com/ams/hack/utils/URIBuilderUtilsTest.java
+++ src/test/java/com/ams/hack/utils/URIBuilderUtilsTest.java
@@ -7,10 +7,10 @@ import static org.junit.Assert.*;
 
 public class URIBuilderUtilsTest {
 
-    private static final String URL_WITH_LOCATION = "https://api.resrobot.se/v2/location.name.json?key=ff90d649-2b80-4bed-919d-cc672472a742&input=stockholm";
-    private final String URL_WITH_ID = "https://api.resrobot.se/v2/trip?key=ff90d649-2b80-4bed-919d-cc672472a742&originId=740000001&destId=740000003&passlist=0&date=2016-10-18&time=07:30&format=json";
-    private final String URL_WITH_COORDINATES = "https://api.resrobot.se/v2/trip?key=ff90d649-2b80-4bed-919d-cc672472a742&originCoordLat=59.330136&originCoordLong=18.058151&destCoordLat=56.898778&destCoordLong=14.556322&passlist=0&date=2016-10-18&time=07:30&format=json";
-    private final String URL_WITH_COORDINATE = "https://api.resrobot.se/v2/location.nearbystops.json?key=ff90d649-2b80-4bed-919d-cc672472a742&originCoordLat=59.330136&originCoordLong=18.058151";
+    private final static String URL_WITH_LOCATION = "https://api.resrobot.se/v2/location.name.json?key=ff90d649-2b80-4bed-919d-cc672472a742&input=stockholm";
+    private final static String URL_WITH_ID = "https://api.resrobot.se/v2/trip?key=ff90d649-2b80-4bed-919d-cc672472a742&originId=740000001&destId=740000003&passlist=0&date=2016-10-18&time=07:30&format=json";
+    private final static String URL_WITH_COORDINATES = "https://api.resrobot.se/v2/trip?key=ff90d649-2b80-4bed-919d-cc672472a742&originCoordLat=59.330136&originCoordLong=18.058151&destCoordLat=56.898778&destCoordLong=14.556322&passlist=0&date=2016-10-18&time=07:30&format=json";
+    private final static String URL_WITH_COORDINATE = "https://api.resrobot.se/v2/location.nearbystops.json?key=ff90d649-2b80-4bed-919d-cc672472a742&originCoordLat=59.330136&originCoordLong=18.058151";
 
     @Test
     public void testBuildURLForIds() throws Exception {
